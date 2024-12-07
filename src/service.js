0909b51098686390ed836db5316ce26b79cedbbb
Jacob Williams <jawoba004@gmail.com>
2024-12-07 10:36:32 -0700

    got rid of the lint waring

--- src/service.js
+++ src/service.js
@@ -135,7 +135,7 @@ app.use('*', (req, res) => {
 });
 
 // Default error handler for all exceptions and errors.
-app.use((err, req, res, next) => {
+app.use((err, req, res, _next) => {  // Add underscore to indicate intentionally unused parameter
   // Log the error with wrapped logger
   logger.log('error', 'service', {
     error: err.message,
